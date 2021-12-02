16c9d0204ac9da27bc05d21738c6cd7d9de9fb06
npsables <phuongnguyen.ng3010@gmail.com>
2021-12-02 19:11:04 +0700

    thing

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,9 @@
 export { default as Navbar } from './Navbar/Navbar';
 export { default as Button } from './Button/Button';
-// export { default as HeroSection } from './HeroSection/HeroSection';
+export { default as HeroSection } from './HeroSection/HeroSection';
 export { default as Home } from './pages/Home';
 export { default as ImportWallet } from './pages/ImportWallet';
 export { default as CreateWallet } from './pages/CreateWallet';
 export { default as HDWallet } from './pages/HDWallet';
 
-export { default as Footer } from './Footer/Footer';
+// export { default as Footer } from './Footer/Footer';
