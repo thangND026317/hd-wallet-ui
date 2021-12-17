0b5fceea3ae9fcf266310f4330240874db0a2508
Thang Nguyen <thang.nguyen.dinh026317@gmail.com>
2021-12-17 21:45:32 +0700

    Create new child

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,20 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactDom from 'react-dom';
 import Button from '../Button/Button';
 
-const Modal = ({ children, onClose }) => {
-  return <>
-    <div className="overlay" />
-    <div className="modal">
-      {children}
-      <Button className='btns'
-        buttonStyle='btn--outline'
-        buttonSize='btn--medium'
-        onClick={onClose}
-      >
-        Cancel
-      </Button>
+import './Modal.css';
+
+const Modal = ({ children, open, onClose, onConfirm }) => {
+  const [input, setInput] = useState('');
+
+  const handleOnChange = event => setInput(event.target.value);
+
+  const handleOnConfirm = (index) => {
+    onConfirm(index);
+    onClose();
+  }
+
+  if (!open) return null;
+
+  return ReactDom.createPortal(<div className="modal-background">
+    <div className="modal-container">
+      <h1>{children}</h1>
+      <br />
+      <label htmlFor='child-index'>Input child index</label>
+      <input
+        id='child-index'
+        type="tel"
+        onChange={handleOnChange}
+        value={input}
+      />
+
+      <div className="modal-btns">
+        <Button className='btn'
+          buttonStyle='btn--outline'
+          buttonSize='btn--medium'
+          onClick={onClose}
+        >
+          Cancel
+        </Button>
+        <Button className='btn'
+          buttonStyle='btn--primary'
+          buttonSize='btn--medium'
+          onClick={() => handleOnConfirm(input)}
+        >
+          Confirm
+        </Button>
+      </div>
+
     </div>
-  </>
+  </div>, document.getElementById("portal-root"))
 }
 
 export default Modal;
--- src/components/pages/HDWallet.js
+++ src/components/pages/HDWallet.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import * as hdcore from 'hdcore-ts';
 import { Tooltip, IconButton } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
@@ -6,37 +6,52 @@ import AddIcon from '@mui/icons-material/Add';
 import AccountList from '../AccountList/AccountList';
 
 import '../../App.css';
+import Modal from '../Modal/Modal';
 
 const DEFAULT_CHILD = 2021;
 
-const HDWallet = (props) => {
-  // const accounts = [];
-  const mnemonic = props.location.mnemonic ? props.location.mnemonic : localStorage.getItem('mnemonic');
+const HDWallet = () => {
+  const [openModal, setOpenModal] = useState(false);
+  const [accounts, setAccounts] = useState([]);
+  const mnemonic = localStorage.getItem('mnemonic');
 
+  // Default wallets
   const seed = hdcore.account.createSeed(mnemonic);
   const master = hdcore.account.createMasterAccount('501', seed);
-  const path = hdcore.account.getPath(0, 501, DEFAULT_CHILD);
 
-  //("m/44'/501'/0'/0'/2021'")
+  const path = hdcore.account.getPath(501, DEFAULT_CHILD); // ("m/44'/501'/0'/0'/2021'")
   const default_child = hdcore.account.createChildAccount('501', seed, path);
 
-  // accounts.push(master);
-  // accounts.push(default_child);
-  //priv={master.prv} 
+  const createChild = (index) => {
+    const newPath = hdcore.account.getPath(501, 2020);
+
+    const newChild = hdcore.account.createChildAccount('501', seed, newPath);
+    accounts.push(newChild);
+    setAccounts(accounts);
+  }
 
   return <div className='hd-wallet'>
-    <div className="hero-container-other">
+    <div className="hero-container">
       <AccountList pub={master.pub} />
       <AccountList pub={default_child.pub} />
+      {accounts.map((account) => <AccountList key={account.pub + 1} pub={account.pub} />)}
 
       <Tooltip title="Add a child wallet" arrow>
         <IconButton
           size="large"
           color="primary"
+          onClick={() => setOpenModal(true)}
         >
           <AddIcon />
         </IconButton>
       </Tooltip>
+
+      <Modal
+        open={openModal}
+        onClose={() => setOpenModal(false)}
+        onConfirm={createChild}
+      >
+        Add a child wallet</Modal>
     </div>
   </div>;
 }
